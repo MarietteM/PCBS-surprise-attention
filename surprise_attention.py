94b70f3d11b7e171bacb4afd0a092085bdc3d6df
Monica Hegde <hegdms0@gmail.com>
2019-01-25 11:04:29 +0100

    ‘doc’

--- surprise_attention.py
+++ surprise_attention.py
@@ -29,20 +29,16 @@ canvas3 = stimuli.Canvas(screensize)
 
 
 ### INSTRUCTIONS ###    ##need to make instructions better...they're not clear rn
-practiceinstructions = """ For this experiment, you will first see a cross, followed by a display of 12 squares 
-in the form of a clock. There will be a letter on each square. Your task is to determine whether there was an "H" 
-or "U" among those letters. You will respond with the left and right keys, marked with "H" and "U" correspondingly.
-
-Let's do some practice trials!
-
-To begin press the space bar"""
+practiceinstructions = """ For this experiment, you will first see a cross, followed by a display of 12 squares in the form of a clock.
+There will be a letter on each square. Your task is to determine whether there was an "H" or "U" among those letters. 
+You will respond with the left and right keys, marked with "H" and "U" correspondingly.
+We wil now do some practice trials! To begin press the space bar"""
 
 expinstructions = """We will now begin the actual experiment. As a reminder:
 
-    You will first see a cross, followed by a display of 12 squares 
-in the form of a clock. There will be a letter on each square. Your task is to determine whether there was an "H" 
-or "U" among those letters. You will respond with the left and right keys, marked with "H" and "U" correspondingly.
-
+You will first see a cross, followed by a display of 12 squares in the form of a clock. 
+There will be a letter on each square. Your task is to determine whether there was an "H" or "U" among those letters.
+You will respond with the left and right keys, marked with "H" and "U" correspondingly.
 To begin press the space bar"""
 
 
@@ -92,6 +88,7 @@ def trial_targetcolor(targetcolor, canvas1, canvas2, canvas3):
     trial.set_factor("Target Letter", str(targetletters[ranHU]))
     trial.set_factor("Response", key)
     trial.set_factor("Reaction Time", rt)
+    
     ##### Setting Target Color Factor --> red vs. green #####
     if targetcolor == (255, 0, 0):
         trial.set_factor("Target Color", "red")        
@@ -107,24 +104,14 @@ def trial_targetcolor(targetcolor, canvas1, canvas2, canvas3):
         trial.set_factor("Accuracy", "miss")
     return trial
 
-    
-    #need to measure RT here
-    ##first 48 targets will be RED, second 48 targets will be GREEN---target colors
-    ##first 12 trials--practice
-    ##if keyboard press is the same as targetletters[ranHU]--> HIT, if not, MISS
-    
 
 #################################### STARTING EXPERIMENT ####################################
 expyriment.control.start(skip_ready_screen = True)
-#stimuli.TextScreen('Instructions', instructions).present()
-#exp.keyboard.wait(constants.K_SPACE)
 exp.data_variable_names = ["Target_Position", "Target_Color", "Target_Letter", "Response", "Reaction Time", "Block", "Accuracy"]
 N_trials_block, N_blocks = 108, 2
 
 
-###PRESENTING STIMULI IN 2 BLOCKS###
-
-for block in range(3):#creating 2 blocks and then
+for block in range(3): ### Three Blocks: Practice, Part1: Conjunction Search Segments, Part 2: Surprise Trials ###
     if block<2:
         INSTRUCTIONS = [practiceinstructions,expinstructions]
         stimuli.TextScreen('Instructions', INSTRUCTIONS[block]).present()
@@ -132,12 +119,12 @@ for block in range(3):#creating 2 blocks and then
     COLOR = [RED, RED, GREEN]
     trial = trial_targetcolor(COLOR[block], canvas1, canvas2, canvas3)
     if block == 0:
-        for trials in range(1): #creating 12 practice trials per block
+        for trials in range(12): #creating 12 practice trials per block
             trial = trial_targetcolor(COLOR[block], canvas1, canvas2, canvas3)
             trial.set_factor("Block", "Practice")
             exp.data.add([trial.get_factor("Target Position"),trial.get_factor("Target Color"),trial.get_factor("Target Letter"), trial.get_factor("Response"),trial.get_factor("Reaction Time"), trial.get_factor("Block"),trial.get_factor("Accuracy")])
     else:
-        for trials in range(2): #creating 12 practice trials per block
+        for trials in range(48): #creating 48 trials per experimental blocks [Conjunction Search Segment; Surprise Trials]
             trial = trial_targetcolor(COLOR[block], canvas1, canvas2, canvas3)
             trial.set_factor("Block", "Block " + str(block))
             exp.data.add([trial.get_factor("Target Position"),trial.get_factor("Target Color"),trial.get_factor("Target Letter"), trial.get_factor("Response"),trial.get_factor("Reaction Time"), trial.get_factor("Block"),trial.get_factor("Accuracy")])
@@ -145,10 +132,3 @@ for block in range(3):#creating 2 blocks and then
 
 expyriment.control.end(goodbye_text = "Thank you for participating!", confirmation = False, goodbye_delay = 1000)
 
-#To-do
-##now need to have it so that the last canvas--squares w/o text AND WAIT!!!
-##figure out how to WAIT until either < or > is pressed
-##figure out how to record responses
-
-##DONE
-### need to make first 48 trials RED and second 48 trials GREEN
